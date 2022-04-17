01ee2aadda5da6fe5e9ca839e8d49354f0f95b75
Daria Turichina <t.dasha5@yandex.ru>
2022-04-17 03:15:58 +0300

    add actions

--- src/roguelike/game_engine/env_manager/env_manager.py
+++ src/roguelike/game_engine/env_manager/env_manager.py
@@ -7,26 +7,59 @@ from roguelike.game_engine.env_manager.map import Map
 from roguelike.game_engine.env_manager.map_objects_storage import Treasure, MapObject, Stats
 
 
+class Drawable:
+    pass
+
+
+class InventoryPresenter(Drawable):
+    """Class that governs the presentation of the Inventory"""
+
+    def __init__(self, treasures: tp.List[Treasure]) -> None:
+        self._treasures = treasures
+        self._current = 0
+
+    def add_treasure(self, treasure: Treasure) -> None:
+        self._treasures.append(treasure)
+
+    def select_next(self) -> None:
+        if self._current + 1 < len(self._treasures):
+            self._current += 1
+
+    def select_previous(self) -> None:
+        if self._current > 0:
+            self._current -= 1
+
+    def get_selected(self) -> tp.Optional[Treasure]:
+        if len(self._treasures) > 0:
+            return self._treasures[self._current]
+        return None
+
+
 class Inventory:
     """Class for describing player's inventory"""
 
     def __init__(self, treasures: tp.List[Treasure]) -> None:
         self._treasure_is_worn = {treasure: False for treasure in treasures}
+        self._presenter = InventoryPresenter(treasures)
 
     def get_additional_stats(self) -> Stats:
-        final_stats = sum(treasure.stats
-                          for treasure, is_on in self._treasure_is_worn.items()
-                          if is_on)
-        return tp.cast(Stats, final_stats)
+        return sum((treasure.stats
+                    for treasure, is_on in self._treasure_is_worn.items()
+                    if is_on), start=Stats(0.0, 0.0))
 
-    def put_on(self, treasure: Treasure) -> None:
-        self._treasure_is_worn[treasure] = True
-
-    def put_off(self, treasure: Treasure) -> None:
-        self._treasure_is_worn[treasure] = False
+    def change_treasure_state(self, treasure: Treasure) -> None:
+        self._treasure_is_worn[treasure] = not self._treasure_is_worn[treasure]
 
     def get_treasures(self) -> tp.Iterable[Treasure]:
         return tuple(self._treasure_is_worn)
+
+    def add_treasure(self, treasure: Treasure) -> None:
+        self._treasure_is_worn[treasure] = False
+        self._presenter.add_treasure(treasure)
+
+    @property
+    def presenter(self) -> InventoryPresenter:
+        return self._presenter
 
 
 @dataclass
