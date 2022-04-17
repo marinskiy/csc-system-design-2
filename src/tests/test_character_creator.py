3a9150a21ec0487512af3a5213c1d1ba0e65690e
Alexander Marinskiy <marinskiy@gmail.com>
2022-04-18 00:41:58 +0300

    name fix

--- src/tests/test_character_creator.py
+++ src/tests/test_character_creator.py
@@ -6,7 +6,7 @@ import os
 from roguelike.game_engine.game_manager.game_constructor.character_creator import CharacterCreator
 
 
-def test_default_character() -> None:
+def test_default_character_creation() -> None:
     player = CharacterCreator.create()
     character_file_path = os.path.join(os.path.dirname(__file__), "../assets/default_character.json")
     with open(character_file_path, encoding="utf-8") as json_file:
