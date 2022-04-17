7ecfe699c28c69f4c813b592a60e8fa3a5e2e794
Alexander Marinskiy <marinskiy@gmail.com>
2022-04-18 00:23:27 +0300

    fix linter

--- src/roguelike/game_engine/game_manager/game_constructor/character_creator.py
+++ src/roguelike/game_engine/game_manager/game_constructor/character_creator.py
@@ -11,8 +11,8 @@ class CharacterCreator:
 
     @staticmethod
     def create() -> PlayerCharacter:
-        character_file_path = os.path.join(os.path.dirname(__file__), '../../../../assets/default_character.json')
-        with open(character_file_path) as json_file:
+        character_file_path = os.path.join(os.path.dirname(__file__), "../../../../assets/default_character.json")
+        with open(character_file_path, encoding="utf-8") as json_file:
             player_data = json.load(json_file)
 
         return PlayerCharacter(Stats(health=player_data["stats"]["health"], attack=player_data["stats"]["attack"]))
