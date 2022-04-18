c9d7aba8aa0daed27359aa5807800aad18f77852
Alexander Marinskiy <marinskiy@gmail.com>
2022-04-18 14:17:02 +0300

    implement loader

--- src/roguelike/game_engine/game_manager/game_constructor/environment_loader.py
+++ src/roguelike/game_engine/game_manager/game_constructor/environment_loader.py
@@ -3,36 +3,89 @@ Contains all classes needed to load game
 """
 
 import typing as tp
-from roguelike.game_engine.env_manager.map import MapCoordinates
-from roguelike.game_engine.env_manager.map_objects_storage import Stats, Obstacle, Treasure, PlayerCharacter
+import json
+
+from roguelike.game_engine.env_manager.env_manager import Environment, Inventory
+from roguelike.game_engine.env_manager.map import MapCoordinates, Map
+from roguelike.game_engine.env_manager.map_objects_storage import Stats, Obstacle, Treasure, PlayerCharacter, MapObject
+from roguelike.game_engine.game_manager.game_processor.game_state import GameState, Mode
 
 
-def _load_coordinates(value: tp.List[int]) -> MapCoordinates:
-    if len(value) != 2:
-        raise ValueError("Invalid coordinates json")
-    return MapCoordinates(value[0], value[1])
+class GameLoader:
+    """Loads GameState from file"""
 
+    @staticmethod
+    def load_game(path: str) -> GameState:
+        with open(path, encoding="utf-8") as json_file:
+            world_data = json.load(json_file)
+        geomap, world_objects, player = GameLoader._load_world(world_data)
+        return GameState(Mode.MAP, Environment(geomap, world_objects), Inventory([]), player)
 
-def _load_stats(value: tp.Dict[str, float]) -> Stats:
-    if len(value) != 2 or "attack" not in value.keys() or "health" not in value.keys():
-        raise ValueError("Invalid stats json")
-    return Stats(value["health"], value["attack"])
+    @staticmethod
+    def _load_coordinates(value: tp.List[int]) -> MapCoordinates:
+        if len(value) != 2:
+            raise ValueError("Invalid coordinates json")
+        return MapCoordinates(value[0], value[1])
 
+    @staticmethod
+    def _load_stats(value: tp.Dict[str, float]) -> Stats:
+        if len(value) != 2 or "attack" not in value.keys() or "health" not in value.keys():
+            raise ValueError("Invalid stats json")
+        return Stats(value["health"], value["attack"])
 
-def _load_obstacle(value: tp.Dict[str, tp.Any]) -> Obstacle:
-    if len(value) != 0:
-        raise ValueError("Invalid obstacle settings json")
-    return Obstacle()
+    @staticmethod
+    def _load_obstacle(value: tp.Dict[str, tp.Any]) -> Obstacle:
+        if len(value) != 0:
+            raise ValueError("Invalid obstacle settings json")
+        return Obstacle()
 
+    @staticmethod
+    def _load_treasure(value: tp.Dict[str, tp.Any]) -> Treasure:
+        if len(value) != 2 or "name" not in value.keys() or "stats" not in value.keys() or \
+                not isinstance(value["name"], str):
+            raise ValueError("Invalid treasure settings json")
+        return Treasure(value["name"], GameLoader._load_stats(value["stats"]))
 
-def _load_treasure(value: tp.Dict[str, tp.Any]) -> Treasure:
-    if len(value) != 2 or "name" not in value.keys() or "stats" not in value.keys() or \
-            not isinstance(value["name"], str):
-        raise ValueError("Invalid treasure settings json")
-    return Treasure(value["name"], _load_stats(value["stats"]))
+    @staticmethod
+    def _load_player(value: tp.Dict[str, tp.Any]) -> PlayerCharacter:
+        if len(value) != 1 or "stats" not in value.keys():
+            raise ValueError("Invalid player settings json")
+        return PlayerCharacter(GameLoader._load_stats(value["stats"]))
 
+    @staticmethod
+    def _load_map(value: tp.Dict[str, int]) -> Map:
+        if len(value) != 2 or "width" not in value.keys() or "height" not in value.keys():
+            raise ValueError("Invalid map json")
+        return Map(value["width"], value["height"])
 
-def _load_player(value: tp.Dict[str, tp.Any]) -> PlayerCharacter:
-    if len(value) != 1 or "stats" not in value.keys():
-        raise ValueError("Invalid player settings json")
-    return PlayerCharacter(_load_stats(value["stats"]))
+    @staticmethod
+    def _load_world(value: tp.Dict[str, tp.Any]) -> tp.Tuple[Map, tp.List[tp.Any], PlayerCharacter]:
+        if len(value) != 2 or "map" not in value.keys() or "objects" not in value.keys() or \
+                not isinstance(value["objects"], list):
+            raise ValueError("Invalid json format")
+
+        geomap = GameLoader._load_map(value["map"])
+        world_objects: tp.List[MapObject] = []
+
+        for map_object_json in value["objects"]:
+            if len(map_object_json) != 3 or "type" not in map_object_json.keys() \
+                    or "pos" not in map_object_json.keys() or \
+                    "settings" not in map_object_json.keys() or \
+                    map_object_json["type"] not in ["player", "obstacle", "treasure"]:
+                raise ValueError("Invalid map object json format")
+
+            coords = GameLoader._load_coordinates(map_object_json["pos"])
+
+            if map_object_json["type"] == "player":
+                world_object = GameLoader._load_player(map_object_json["settings"])
+            elif map_object_json["type"] == "obstacle":
+                world_object = GameLoader._load_obstacle(map_object_json["settings"])
+            elif map_object_json["type"] == "treasure":
+                world_object = GameLoader._load_treasure(map_object_json["settings"])
+
+            world_objects.append(world_object)
+            if isinstance(world_object, PlayerCharacter):
+                player = world_object
+            geomap.add_object(coords, world_object)
+
+        return geomap, world_objects, player
--- src/tests/test_environment_loader.py
+++ src/tests/test_environment_loader.py
@@ -1,55 +1,80 @@
 """Env loader tests"""
 
 import pytest
+import os
 from roguelike.game_engine.env_manager.map import MapCoordinates
-from roguelike.game_engine.env_manager.map_objects_storage import Stats, Obstacle
-from roguelike.game_engine.game_manager.game_constructor.environment_loader import _load_coordinates, _load_stats, \
-    _load_obstacle, _load_treasure, _load_player
+from roguelike.game_engine.env_manager.map_objects_storage import Stats, Obstacle, Treasure, PlayerCharacter
+from roguelike.game_engine.game_manager.game_constructor.environment_loader import GameLoader
 
 
 def test_coordinates_load_correctly() -> None:
-    assert _load_coordinates([0, 1]) == MapCoordinates(0, 1)
+    assert GameLoader._load_coordinates([0, 1]) == MapCoordinates(0, 1)  # pylint: disable=W0212
 
     with pytest.raises(ValueError):
-        _load_coordinates([])
+        GameLoader._load_coordinates([])  # pylint: disable=W0212
     with pytest.raises(ValueError):
-        _load_coordinates([0])
+        GameLoader._load_coordinates([0])  # pylint: disable=W0212
     with pytest.raises(ValueError):
-        _load_coordinates([0, 1, 2])
+        GameLoader._load_coordinates([0, 1, 2])  # pylint: disable=W0212
 
 
 def test_stats_load_correctly() -> None:
-    assert _load_stats({"health": 100.0, "attack": 100.0}) == Stats(health=100.0, attack=100.0)
+    assert GameLoader._load_stats(  # pylint: disable=W0212
+        {"health": 100.0, "attack": 100.0}) == Stats(health=100.0, attack=100.0)
 
     with pytest.raises(ValueError):
-        _load_stats({})
+        GameLoader._load_stats({})  # pylint: disable=W0212
     with pytest.raises(ValueError):
-        _load_stats({"health": 100.0, "attack": 100.0, "defence": 100.0})
+        GameLoader._load_stats({"health": 100.0, "attack": 100.0, "defence": 100.0})  # pylint: disable=W0212
 
 
 def test_obstacle_load_correctly() -> None:
-    assert _load_obstacle({}) == Obstacle()
+    assert GameLoader._load_obstacle({}) == Obstacle()  # pylint: disable=W0212
 
     with pytest.raises(ValueError):
-        _load_obstacle({"health": 100.0, "attack": 100.0, "defence": 100.0})
+        GameLoader._load_obstacle({"health": 100.0, "attack": 100.0, "defence": 100.0})  # pylint: disable=W0212
 
 
 def test_treasure_load_correctly() -> None:
-    test_treasure = _load_treasure({"name": "Super Helmet", "stats": {"health": 10.0, "attack": 0.0}})
+    test_treasure = GameLoader._load_treasure(  # pylint: disable=W0212
+        {"name": "Super Helmet", "stats": {"health": 10.0, "attack": 0.0}})
     assert test_treasure.name == "Super Helmet"
     assert test_treasure.stats == Stats(10.0, 0.0)
 
     with pytest.raises(ValueError):
-        _load_treasure({})
+        GameLoader._load_treasure({})  # pylint: disable=W0212
     with pytest.raises(ValueError):
-        _load_treasure({"health": 100.0, "attack": 100.0, "defence": 100.0})
+        GameLoader._load_treasure({"health": 100.0, "attack": 100.0, "defence": 100.0})  # pylint: disable=W0212
 
 
 def test_player_load_correctly() -> None:
-    test_player = _load_player({"stats": {"health": 10.0, "attack": 0.0}})
+    test_player = GameLoader._load_player({"stats": {"health": 10.0, "attack": 0.0}})  # pylint: disable=W0212
     assert test_player.stats == Stats(10.0, 0.0)
 
     with pytest.raises(ValueError):
-        _load_player({})
+        GameLoader._load_player({})  # pylint: disable=W0212
     with pytest.raises(ValueError):
-        _load_player({"name": "Super Helmet", "stats": {"health": 10.0, "attack": 0.0}})
+        GameLoader._load_player(  # pylint: disable=W0212
+            {"name": "Super Helmet", "stats": {"health": 10.0, "attack": 0.0}})
+
+
+def test_map_load_correctly() -> None:
+    test_map = GameLoader._load_map({"width": 60, "height": 40})  # pylint: disable=W0212+
+
+    assert test_map.get_dimensions() == (60, 40)
+
+    with pytest.raises(ValueError):
+        GameLoader._load_map({})  # pylint: disable=W0212
+
+
+def test_load_from_file() -> None:
+    file_path = os.path.join(os.path.dirname(__file__), "test_game.json")
+    state = GameLoader.load_game(file_path)
+
+    assert state.player.stats == Stats(100.0, 100.0)
+
+    assert isinstance(state.environment.map.get_objects(MapCoordinates(0, 0))[0], Obstacle)
+    assert isinstance(state.environment.map.get_objects(MapCoordinates(7, 7))[0], Obstacle)
+    assert isinstance(state.environment.map.get_objects(MapCoordinates(20, 20))[0], Treasure)
+    assert isinstance(state.environment.map.get_objects(MapCoordinates(0, 20))[0], Treasure)
+    assert isinstance(state.environment.map.get_objects(MapCoordinates(10, 10))[0], PlayerCharacter)
