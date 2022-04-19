e7beb89195c0c6dc45248d3ceeba21c8d08d90b9
Alexander Marinskiy <marinskiy@gmail.com>
2022-04-19 23:05:07 +0300

    introduce DEFAULT_MAP

--- src/roguelike/__main__.py
+++ src/roguelike/__main__.py
@@ -8,8 +8,10 @@ from roguelike.game_engine.game_manager.game_constructor.game_loader import Game
 from roguelike.game_engine.game_manager.game_processor.game_loop import GameLoop
 from roguelike.ui.keyboard_interpreter import KeyboardInterpreter
 
+DEFAULT_MAP = 'assets/maps/forest.json'
+
 if __name__ == '__main__':
-    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'assets/maps/forest.json')
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), DEFAULT_MAP)
     current_state = GameLoader.load_game(file_path)
     loop = GameLoop(current_state)
 
