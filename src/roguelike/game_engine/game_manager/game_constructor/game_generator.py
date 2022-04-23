72ce2fa5f692b3e71d15adc6e1729d76fe1a3c82
Alexander Marinskiy <marinskiy@gmail.com>
2022-04-23 20:52:06 +0300

    add generators

--- src/roguelike/game_engine/game_manager/game_constructor/game_generator.py
+++ src/roguelike/game_engine/game_manager/game_constructor/game_generator.py
@@ -7,27 +7,147 @@ import random
 
 from roguelike.game_engine.env_manager.map import Map
 from roguelike.game_engine.game_manager.game_constructor.game_loader import check_dict_fields
+from roguelike.game_engine.env_manager.map_objects_storage import Stats, PlayerCharacter, Obstacle, Treasure, MapObject
+
+
+def get_random_value_from_range(value: tp.List[tp.Union[int, float]]) -> int:
+    if len(value) != 2 or value[0] > value[1]:
+        raise ValueError("Invalid value range")
+
+    if isinstance(value[0], int):
+        return random.randint(value[0], value[1])
+    return random.uniform(value[0], value[1])
+
+
+class StatsGenerator:
+    """Produces Stats based on settings"""
+
+    def __init__(self, settings: tp.Dict[str, tp.List[float]]) -> None:
+        self._validate_input(settings)
+        self.health_range = settings["health"]
+        self.attack_range = settings["attack"]
+
+    @staticmethod
+    def _validate_input(settings: tp.Dict[str, tp.List[float]]) -> None:
+        if not check_dict_fields(settings, ["attack", "health"]):
+            raise ValueError("Invalid stats settings json")
+
+    def generate(self) -> Stats:
+        health = get_random_value_from_range(self.health_range)
+        attack = get_random_value_from_range(self.attack_range)
+        return Stats(health, attack)
+
+
+class PlayerCharacterGenerator:
+    """Produces PlayerCharacter based on settings"""
+
+    def __init__(self, settings: tp.Dict[str, tp.Any]) -> None:
+        self._validate_input(settings)
+        self.stats_generator = StatsGenerator(settings["stats"])
+
+    @staticmethod
+    def _validate_input(settings: tp.Dict[str, tp.Any]) -> None:
+        if not check_dict_fields(settings, ["stats"]):
+            raise ValueError("Invalid player settings json")
+
+    def generate(self) -> PlayerCharacter:
+        return PlayerCharacter(self.stats_generator.generate())
+
+
+class ObstacleGenerator:
+    """Produces Obstacle based on settings"""
+
+    def __init__(self, settings: tp.Dict[str, tp.Any]) -> None:
+        self._validate_input(settings)
+
+    @staticmethod
+    def _validate_input(settings: tp.Dict[str, tp.Any]) -> None:
+        if len(settings) != 0:
+            raise ValueError("Invalid obstacle settings json")
+
+    def generate(self) -> Obstacle:
+        return Obstacle()
+
+
+class TreasureGenerator:
+    """Produces Treasure based on settings"""
+
+    def __init__(self, settings: tp.Dict[str, tp.Any]) -> None:
+        self._validate_input(settings)
+        self.names_list = settings["names"]
+        self.stats_generator = StatsGenerator(settings["stats"])
+
+    @staticmethod
+    def _validate_input(settings: tp.Dict[str, tp.Any]) -> None:
+        if not check_dict_fields(settings, ["names", "stats"]) or not isinstance(settings["names"], list):
+            raise ValueError("Invalid treasure settings json")
+
+    def generate(self) -> Treasure:
+        return Treasure(random.choice(self.names_list), self.stats_generator.generate())
+
+
+class MapObjectGenerator:
+    """Produces MapObject based on settings"""
+
+    def __init__(self, settings: tp.Dict[str, tp.Any]) -> None:
+        self._validate_input(settings)
+        self.player_character_generator = PlayerCharacterGenerator(settings["player"])
+        self.obstacle_generator = ObstacleGenerator(settings["obstacle"])
+        self.treasure_generator = TreasureGenerator(settings["treasure"])
+
+    @staticmethod
+    def _validate_input(settings: tp.Dict[str, tp.Any]) -> None:
+        if not check_dict_fields(settings, ["player", "obstacle", "treasure"]):
+            raise ValueError("Invalid treasure settings json")
+
+    def generate(self, key: str) -> MapObject:
+        if key == "player":
+            return self.player_character_generator.generate()
+        elif key == "obstacle":
+            return self.obstacle_generator.generate()
+        elif key == "treasure":
+            return self.treasure_generator.generate()
+        else:
+            raise ValueError("No such MapObject")
+
+
+class MapObjectWheel:
+    """Chooses map object based on weights"""
+
+    def __init__(self, settings: tp.Dict[str, tp.List[tp.Any]]) -> None:
+        self._validate_input(settings)
+        self.population = settings["population"]
+        self.weights = settings["weights"]
+
+    def get_next_object_type(self) -> str:
+        return random.choices(self.population, self.weights)[0]
+
+    @staticmethod
+    def _validate_input(settings: tp.Dict[str, tp.List[tp.Any]]) -> None:
+        if not check_dict_fields(settings, ["population", "weights"]) or len(
+                settings["population"]) == 0 or len(settings["population"]) != len(settings["weights"]):
+            raise ValueError("Invalid wheel settings json")
+
+
+class MapGenerator:
+    """Produces Map based on settings"""
+
+    def __init__(self, settings: tp.Dict[str, tp.List[int]]) -> None:
+        self._validate_input(settings)
+        self.width_range = settings["width"]
+        self.height_range = settings["height"]
+
+    def generate(self) -> Map:
+        width = get_random_value_from_range(self.width_range)
+        height = get_random_value_from_range(self.height_range)
+        return Map(width, height)
+
+    @staticmethod
+    def _validate_input(settings: tp.Dict[str, tp.List[int]]) -> None:
+        if not check_dict_fields(settings, ["width", "height"]):
+            raise ValueError("Invalid default map json")
 
 
 class GameGenerator:
     """Generates GameState based on default settings"""
 
-    @staticmethod
-    def _get_random_value_from_range(value: tp.List[int]) -> int:
-        if len(value) != 2 or value[0] > value[1]:
-            raise ValueError("Invalid value range")
-        return random.randint(value[0], value[1])
-
-    @staticmethod
-    def _decide_from_probability(value: float) -> bool:
-        if value < 0 or value > 1:
-            raise ValueError("Invalid probability")
-        return random.random() < value
-
-    @staticmethod
-    def _generate_map(value: tp.Dict[str, tp.List[int]]) -> Map:
-        if not check_dict_fields(value, ["width", "height"]):
-            raise ValueError("Invalid map json")
-        width = GameGenerator._get_random_value_from_range(value["width"])
-        height = GameGenerator._get_random_value_from_range(value["height"])
-        return Map(width, height)
--- src/tests/test_game_generator.py
+++ src/tests/test_game_generator.py
@@ -2,32 +2,111 @@
 
 import pytest
 
-from roguelike.game_engine.game_manager.game_constructor.game_generator import GameGenerator
+from roguelike.game_engine.env_manager.map_objects_storage import Obstacle
+from roguelike.game_engine.game_manager.game_constructor.game_generator import GameGenerator, StatsGenerator, \
+    PlayerCharacterGenerator, get_random_value_from_range, ObstacleGenerator, TreasureGenerator, MapObjectGenerator, \
+    MapObjectWheel, MapGenerator
 
 
 def test_random_value_from_range() -> None:
-    test_value = GameGenerator._get_random_value_from_range([0, 100])  # pylint: disable=W0212
+    test_value = get_random_value_from_range([0, 100])
     assert 0 <= test_value <= 100
 
-    test_value = GameGenerator._get_random_value_from_range([0, 0])  # pylint: disable=W0212
+    test_value = get_random_value_from_range([0.1, 0.5])
+    assert 0.1 <= test_value <= 0.5
+
+    test_value = get_random_value_from_range([0, 0])
     assert test_value == 0
 
     with pytest.raises(ValueError):
-        GameGenerator._get_random_value_from_range([100, 0])  # pylint: disable=W0212
+        get_random_value_from_range([100, 0])
+    with pytest.raises(ValueError):
+        get_random_value_from_range([0.5, 0.1])
 
 
-def test_decide_from_probability() -> None:
-    assert not GameGenerator._decide_from_probability(0)  # pylint: disable=W0212
-    assert GameGenerator._decide_from_probability(1)  # pylint: disable=W0212
+def test_stats_generated_correctly() -> None:
+    test_stats = StatsGenerator({"health": [100.0, 150.0], "attack": [10.0, 20.0]}).generate()
+
+    assert 100.0 <= test_stats.health <= 150.0
+    assert 10.0 <= test_stats.attack <= 20.0
+
+    with pytest.raises(ValueError):
+        StatsGenerator({})
+    with pytest.raises(ValueError):
+        StatsGenerator({"health": [100.0, 110.0], "attack": [100.0, 150.0], "defence": [100.0, 200.0]})
 
 
-def test_generate_map() -> None:
-    test_map = GameGenerator._generate_map({"width": [30, 100], "height": [1, 10]})  # pylint: disable=W0212+
+def test_player_generated_correctly() -> None:
+    test_player = PlayerCharacterGenerator(  # pylint: disable=W0212
+        {"stats": {"health": [10.0, 20.0], "attack": [0.0, 100]}}).generate()
+
+    assert 10.0 <= test_player.stats.health <= 20.0
+    assert 0.0 <= test_player.stats.attack <= 100.0
+
+    with pytest.raises(ValueError):
+        PlayerCharacterGenerator({})
+    with pytest.raises(ValueError):
+        PlayerCharacterGenerator(
+            {"name": "Super Helmet", "stats": {"health": [10.0, 20.0], "attack": [0.0, 100]}})
+
+
+def test_obstacle_generated_correctly() -> None:
+    assert ObstacleGenerator({}).generate() == Obstacle()
+
+    with pytest.raises(ValueError):
+        ObstacleGenerator({"health": 100.0, "attack": 100.0, "defence": 100.0})
+
+
+def test_treasure_generated_correctly() -> None:
+    test_treasure = TreasureGenerator(
+        {"names": ["Helmet", "Boots"], "stats": {"health": [10.0, 20.0], "attack": [0.0, 30]}}).generate()
+    assert test_treasure.name in ["Helmet", "Boots"]
+    assert 10.0 <= test_treasure.stats.health <= 20.0
+
+    with pytest.raises(ValueError):
+        TreasureGenerator({})
+    with pytest.raises(ValueError):
+        TreasureGenerator({"health": 100.0, "attack": 100.0, "defence": 100.0})
+
+
+def test_map_object_generated_correctly() -> None:
+    object_generator = MapObjectGenerator(
+        {"player": {"stats": {"health": [90.0, 110.0], "attack": [90.0, 110.0]}}, "obstacle": {},
+         "treasure": {"names": ["helmet", "boots", "armor"], "stats": {"health": [-10, 30], "attack": [-10, 30]}}
+         }
+    )
+
+    test_player = object_generator.generate("player")
+    assert 90.0 <= test_player.stats.health <= 110.0
+    assert 90.0 <= test_player.stats.attack <= 110.0
+
+    test_obstacle = object_generator.generate("obstacle")
+    assert test_obstacle == Obstacle()
+
+    test_treasure = object_generator.generate("treasure")
+    assert test_treasure.name in ["helmet", "boots", "armor"]
+    assert -10 <= test_treasure.stats.health <= 30.0
+    assert -10 <= test_treasure.stats.attack <= 30.0
+
+    with pytest.raises(ValueError):
+        object_generator.generate("none")
+
+
+def test_wheel() -> None:
+    wheel = MapObjectWheel({"population": ["obstacle", "treasure", "none"], "weights": [10, 5, 85]})
+    assert wheel.get_next_object_type() in ["obstacle", "treasure", "none"]
+
+    wheel = MapObjectWheel({"population": ["obstacle", "treasure", "none"], "weights": [100, 0, 0]})
+    assert wheel.get_next_object_type() == "obstacle"
+
+    with pytest.raises(ValueError):
+        MapObjectWheel({})
+    with pytest.raises(ValueError):
+        MapObjectWheel({"population": ["obstacle", "treasure", "none"], "weights": [100, 0, 0, 10]})
+
+
+def test_map_generated_correctly() -> None:
+    test_map = MapGenerator({"width": [30, 100], "height": [1, 10]}).generate()
 
     assert 30 <= test_map._width <= 100  # pylint: disable=W0212+
     assert 1 <= test_map._height <= 10  # pylint: disable=W0212+
-
-    with pytest.raises(ValueError):
-        GameGenerator._generate_map({"width": [0, 0], "height": [1, 10]})  # pylint: disable=W0212
-    with pytest.raises(ValueError):
-        GameGenerator._generate_map({"width": [30, 100], "height": [0, 0]})  # pylint: disable=W0212
