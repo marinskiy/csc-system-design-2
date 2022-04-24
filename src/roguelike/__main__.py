10f840f474302363dc14848e67ccda4874237c76
Alexander Marinskiy <marinskiy@gmail.com>
2022-04-24 15:48:39 +0300

    fix mypy

--- src/roguelike/__main__.py
+++ src/roguelike/__main__.py
@@ -4,6 +4,7 @@ Contains game main loop
 
 import os
 import argparse
+import typing as tp
 
 from roguelike.game_engine.game_manager import GameLoop, GameLoader
 from roguelike.game_engine.game_manager.game_constructor.game_generator import GameGenerator
@@ -11,13 +12,13 @@ from roguelike.game_engine.game_manager.game_processor.game_state import GameSta
 from roguelike.ui.keyboard_interpreter import KeyboardInterpreter
 
 
-def parse_arguments():
+def parse_arguments() -> tp.Any:
     parser = argparse.ArgumentParser(description="Game command line arguments.")
     parser.add_argument("-path", type=map_path, help="path to the map")
     return parser.parse_args()
 
 
-def map_path(path):
+def map_path(path: str) -> str:
     if os.path.isfile(path) and path.endswith(".json"):
         return path
     else:
