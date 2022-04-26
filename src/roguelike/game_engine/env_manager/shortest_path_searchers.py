fdc8eff5820c42733fd6fd0410a2d39478847a8a
star.yar <vestnik.yar@gmail.com>
2022-04-27 00:55:50 +0600

    Refactor

--- src/roguelike/game_engine/env_manager/shortest_path_searchers.py
+++ src/roguelike/game_engine/env_manager/shortest_path_searchers.py
@@ -5,6 +5,7 @@ import heapq
 import typing as tp
 
 T = tp.TypeVar('T')
+TPath = tp.Dict['Coordinates', 'Coordinates']
 
 
 class Graph(tp.Protocol):
@@ -22,6 +23,7 @@ class Coordinates(tp.Protocol):
 
 class PriorityQueue(tp.Generic[T]):
     """Standard priority queue"""
+
     def __init__(self) -> None:
         self.elements: tp.List[tp.Tuple[float, T]] = []
 
@@ -39,22 +41,26 @@ def eval_distance_using_heuristic(a: Coordinates, b: Coordinates) -> int:
     return abs(a.x - b.x) + abs(a.y - b.y)
 
 
-def search_using_a_star(graph: Graph, start: Coordinates, goal: Coordinates):
+def search_using_a_star(
+        graph: Graph,
+        start: Coordinates,
+        destination: Coordinates,
+) -> tp.Tuple[TPath, tp.Dict[Coordinates, int]]:
     frontier: PriorityQueue[Coordinates] = PriorityQueue()
     frontier.put(start, 0)
-    came_from: tp.Dict[Coordinates, tp.Optional[Coordinates]] = {start: None}
+    came_from: TPath = {start: start}
     cost_so_far = {start: 0}
 
     while not frontier.empty():
         current = frontier.get()
-        if current == goal:
+        if current == destination:
             break
         for next_ in graph.get_neighbours(current):
             new_cost = cost_so_far[current] + 1
             is_first_visit = next_ not in cost_so_far
             if is_first_visit or new_cost < cost_so_far[next_]:
                 cost_so_far[next_] = new_cost
-                priority = new_cost + eval_distance_using_heuristic(next_, goal)
+                priority = new_cost + eval_distance_using_heuristic(next_, destination)
                 frontier.put(next_, priority)
                 came_from[next_] = current
     return came_from, cost_so_far
